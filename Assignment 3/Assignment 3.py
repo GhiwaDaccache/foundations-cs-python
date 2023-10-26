1f605825bc219b7f24d97b68d98c4fea419a8787
GhiwaDaccache <ghiwadaccache4@gmail.com>
2023-10-26 20:51:36 +0300

    addMatrices first try

--- Assignment 3/Assignment 3.py
+++ Assignment 3/Assignment 3.py
@@ -10,4 +10,19 @@ def displayMenu():
           "\t5. Check Palindrome\n" + 
           "\t6. Search for an Element & Merge Sort\n" + "\t7. Exit\n"
               )
+# displayMenu function here, prints out all the options the user can choose from.
+def addMatrices():
+    main_matrix = []
+    rows = int(input("Enter the number of rows: "))
+    columns = int(input("Enter the number of columns: "))
+    for row in range(rows):
+        main_matrix.append([])
+        for column in range(columns):
+            numbers = int(input("Enter the elements of your matrix: "))
+    print(main_matrix)
+            
+addMatrices()
 
+        
+        
+        
