688171c06961fa1a7bf1ab932c53661261c4b5aa
GhiwaDaccache <ghiwadaccache4@gmail.com>
2023-10-26 19:59:12 +0300

    Creating displayMenu

--- Assignment 3/Assignment 3.py
+++ Assignment 3/Assignment 3.py
@@ -1,4 +1,13 @@
 print("Assignment 3")
 
 user_name = input("Enter your name here: ")
+    
+def displayMenu():
+    print(
+        "This is the menu: \n" + "\t1. Add Matrices\n" + 
+          "\t2. Check Rotation\n" + "\t3. Invert Directory\n" + 
+          "\t4. Convert Matrix to Dictionary\n" + 
+          "\t5. Check Palindrome\n" + 
+          "\t6. Search for an Element & Merge Sort\n" + "\t7. Exit\n"
+              )
 
