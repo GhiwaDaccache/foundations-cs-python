14d8e7874981ae4c3bd7d53c636e8688bd6a4106
GhiwaDaccache <ghiwadaccache4@gmail.com>
2023-10-27 12:03:46 +0300

    addMatrcies working

--- Assignment 3/Assignment 3.py
+++ Assignment 3/Assignment 3.py
@@ -11,18 +11,46 @@ def displayMenu():
           "\t6. Search for an Element & Merge Sort\n" + "\t7. Exit\n"
               )
 # displayMenu function here, prints out all the options the user can choose from.
+
 def addMatrices():
     matrix1 = []
-    rows = int(input("Enter the number of rows: "))
-    columns = int(input("Enter the number of columns: "))
+    rows = int(input("Enter the number of rows for your matrices: "))
+    columns = int(input("Enter the number of columns for your matrices: "))
     for row in range(rows):
         matrix1.append([])
         for column in range(columns):
-            numbers = int(input("Enter elements of your matrix: "))
-    print(matrix1)
+            print("Enter element", column, "of matrix", row, "in main matrix 1")
+            numbers = int(input())
+            matrix1[row].append(numbers)
+    print()        
+    print("Your first matrix is:\n", matrix1)
+    print()
+    
+    matrix2 = []
+    for row in range(rows):
+        matrix2.append([])
+        for column in range(columns):
+            print("Enter element", column, "of matrix", row, "in main matrix 2")
+            numbers2 = int(input())
+            matrix2[row].append(numbers2)
             
+    print()        
+    print("Your second matrix is:\n", matrix2)
+    print()  
+    
+    matrix = []
+    for row in range(rows):
+        result = []
+        for column in range(columns):
+            result.append(matrix1[row][column] + matrix2[row][column])
+        matrix.append(result)
+    print(matrix)
+    
+    
 addMatrices()
 
-        
+      
+
+  
         
         
