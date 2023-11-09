5e3a221640de93513b77b17e5eaf6b1781eb82c2
GhiwaDaccache <ghiwadaccache4@gmail.com>
2023-11-09 23:33:43 +0200

    close tab delete function

--- Midterm/Ghiwa El Daccache - Midterm Project.py
+++ Midterm/Ghiwa El Daccache - Midterm Project.py
@@ -2,6 +2,9 @@ print("Midterm Project - Ghiwa El Daccache")
 print("-----------------------------------")
 print()
 
+
+open_tabs = []
+
 def displayMenu():
     print(
           "This is the menu. Please make a selection from the following: \n" +
@@ -13,12 +16,26 @@ def displayMenu():
           )
         
 def openTab():
-  print("New tab")
-  input("Please enter the title of the website: ")
-  input("Please enter the URL of the website: ")
+    print()
+    print("New tab")
+    title = input("Please enter the title of the website: ")
+    url = input("Please enter the URL of the website: ")
+    tab = {"Title": title, "URL": url}
+    open_tabs.append(tab)
+    print("New tab opened for:", title)
 
+# If the admin chooses (2), the system should permit the user to input the index of the
+# tab they wish to close. If no index is provided, the system will close the last opened
+# tab.
+                    
+def closeTab():
+    tab_index = int(input("Please enter the index of the tab you wish to close: "))
+    open_tabs[0: tab_index]
+    
+    del open_tabs[tab_index]
+        
 
-openTab()  
+  
 
 def main():
     print("Hello and welcome!")
