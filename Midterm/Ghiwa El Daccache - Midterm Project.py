3dfabe52302f382c4d9c457453baaa6347c85c44
GhiwaDaccache <ghiwadaccache4@gmail.com>
2023-11-12 00:10:52 +0200

    added checkValidInput

--- Midterm/Ghiwa El Daccache - Midterm Project.py
+++ Midterm/Ghiwa El Daccache - Midterm Project.py
@@ -19,12 +19,12 @@ def displayMenu():
         
 def openTab(parent_index):
     print()
-    print("New tab")
 
 # This if condition checks if the opened tab is a nested or a parent tab. 
 # When the user chooses 1, the function will be called with none passed as parameter.
 # When the user chooses 5, the user will be prompted to enter the index of the parent tab then it will be passed to the function. 
     if parent_index == None:
+        print("New tab")
         title = input("Please enter the title of the website: ")
         web_url = input("Please enter the URL of the website: ")
         tab = {"Title": title, "URL": web_url}
@@ -32,6 +32,7 @@ def openTab(parent_index):
         print("New tab opened for:", title)
         
     else:
+        print("New nested tab")
         title = input("Please enter the title of the website: ")
         web_url = input("Please enter the URL of the website: ")
         tab = {"Title": title, "URL": web_url, "parent tab": parent_index}
@@ -41,40 +42,30 @@ def openTab(parent_index):
         open_tabs.append(tab)
         print("New nested tab opened for:", title)
 
+def checkValidInput(index):
+    print()
+# This if statement handles the case where the user doesn't enter an index, it gives it a value of -1 to use later on.
+# If the user doesn't return an empty input, the program enters the while loop.  
+    if index == "":
+        index = -1
 
-########################################################################################################
-# def checkValidInput(index):
-#     if index == "":
-#         index = -1
-#     else:
-#         while index.isdigit() == False or int(index) < -1 or int(index) > len (open_tabs) -1:
-#             print("Invalid input, please enter a valid index (>=0 or < number of open tabs)") 
-#             checkValidInput()
-########################################################################################################
-            
-
+# The while loop here, ensures that the user input is valid by checking if:
+    # The input is a number
+    # The input is a number <-1
+    # The input is a number < number of open tabs (or len(open_tabs) -1)
+# Once the input is valid, the loop is exited and casts the input as an integer.        
+        
+    else:           
+        while index.isdigit() == False or int(index) < -1 or int(index) > len(open_tabs) -1: 
+           print("Invalid input, please enter a valid index (>=0 or < number of open tabs)") 
+           index = input()
+    return int(index)
+           
 def closeTab():
     print()
 # First the user is prompted to enter the index of the tab they wish to close.
     close_index = input("Please enter the index of the tab you wish to close: ")
-
-# This if statement handles the case where the user doesn't enter an index, it gives it a value of -1 to use later on.
-# If the user doesn't return an empty input, the program enters the while loop. 
-    if close_index == "":
-        close_index = -1
-        
-# The while loop here, ensures that the user input is valid by checking if:
-    # The input is a number
-    # To check if the user input is a number, I used .isdigit(), https://www.w3schools.com/python/ref_string_isdigit.asp 
-    # This method returns True if all characters of a string are digits. Checking if it's a digit is important to first verify that it's a valid input and second to cast it later on.
-    # The input is a number <-1
-    # The input is a number < number of open tabs (or len(open_tabs) -1)
-# Once the input is valid, the loop is exited and casts the input as an integer.
-    else:           
-        while close_index.isdigit() == False or int(close_index) < -1 or int(close_index) > len(open_tabs) -1: 
-           print("Invalid input, please enter a valid index (>=0 or < number of open tabs)") 
-           close_index = input("Please enter the index of the tab you wish to close: ")
-        close_index = int(close_index)
+    close_index = checkValidInput(close_index)
 
 # https://www.geeksforgeeks.org/python-del-to-delete-objects/
 # I used the del function which I read about in the article referenced above from geeksforgeeks.com
@@ -88,29 +79,13 @@ def closeTab():
 def switchTab():
     print()
 # First the user is prompted to enter the index of the tab they wish to display.
-    display_index = input("Please enter the index of the tab you wish to switch to: ")
-
-# This if statement handles the case where the user doesn't enter an index, it gives it a value of -1 to use later on.
-# If the user doesn't return an empty input, the program enters the while loop.  
-    if display_index == "":
-        display_index = -1
-        
-# The while loop here, ensures that the user input is valid by checking if:
-    # The input is a number
-    # The input is a number <-1
-    # The input is a number < number of open tabs (or len(open_tabs) -1)
-# Once the input is valid, the loop is exited and casts the input as an integer.
-    else:           
-        while display_index.isdigit() == False or int(display_index) < -1 or int(display_index) > len(open_tabs): 
-           print("Invalid input, please enter a valid index (>=0 or < number of open tabs)") 
-           display_index = input("Please enter the index of the tab you wish to switch to: ")
-        display_index = int(display_index)
-
+    switch_index = input("Please enter the index of the tab you wish to switch to: ")
+    switch_index = checkValidInput(switch_index)
 # The if statement below, checks if the input is -1, means the user did not enter any value, so it switches to the last tab opened, otherwise it switches to the specified one.     
-    if display_index == -1: 
+    if switch_index == -1: 
         url = open_tabs[len(open_tabs) -1]["URL"] 
     else: 
-        url = open_tabs[display_index]["URL"]
+        url = open_tabs[switch_index]["URL"]
 
 # The below block is to print the HTML content of the URL. 
 # Reference: https://realpython.com/python-web-scraping-practical-introduction/
@@ -129,31 +104,44 @@ def displayTabs():
     for tab in open_tabs:
         print(tab["Title"], "is an open tab.")
 
-    
-# the system should enable users to create nested tabs by
-# specifying the index of the parent tab where they want to insert additional tabs.
-# After entering the index, the system should prompt the user to input the titles and
-# contents for the new tabs.
 
-# openTab()
-# openTab()
-# openTab()
-# print()
-# print(open_tabs)
-# print()
-# closeTab()
-# print()
-# print(open_tabs)
-# print()
-# switchTab()
-# displayTabs()
-# print(open_tabs)
+openTab(None)
+openTab(None)
+openTab(None)
+
+print()
+print(open_tabs)
+print()
+
+closeTab()
+print()
+print(open_tabs)
+print()
+
+print()
+switchTab()
+print()
+
+openTab(2)
+print()
+print(open_tabs)
+
+
+def clearTabs(): 
+    pass
+
+def saveTabs():
+    pass
+
+def importTabs():
+    pass
+
 
 
 def main():
     print("Hello and welcome!")
     displayMenu()
-    selection = int(input("Please choose an action from the menu, by entering its assigned number. "))
+    selection = int(input())
     while selection != 9 :
         
         if selection == 1 :
@@ -179,13 +167,13 @@ def main():
             saveTabs()
         
         elif selection == 8 :
-            importTabs()
+            importTabs()  
             
         else:
             print("Please choose a number from the list")
         print()
         displayMenu()
-        selection = int(input("Please choose an action from the menu, by entering its assigned number. "))
+        selection = int(input())
     print("You exited")
     
 
