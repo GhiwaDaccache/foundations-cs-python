8dcd9731a8953fb4cf0a28771f566c041915b753
GhiwaDaccache <ghiwadaccache4@gmail.com>
2023-11-22 16:24:47 +0200

    Menu added

---
+++ Assignment 4/Assignment 4.py
@@ -0,0 +1,14 @@
+##############
+# Assignment 4
+##############
+
+def displayMenu():
+    print("Please select a number from the following: \n",
+        "\n\t1. Singly Linked List\n",
+        "\t2. Check if Palindrome\n",
+        "\t3. Priority Queue\n",
+        "\t4. Evaluate an Infix Expression\n",
+        "\t5. Graph\n",
+        "\t6. Exit"
+        )
+
