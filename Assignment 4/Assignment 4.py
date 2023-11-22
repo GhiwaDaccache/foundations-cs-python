49cf7da0ee99815152f893af875d1d75d9caff33
GhiwaDaccache <ghiwadaccache4@gmail.com>
2023-11-22 21:47:16 +0200

    initial commit

---
+++ Assignment 4/Assignment 4.py
@@ -0,0 +1,19 @@
+<<<<<<< HEAD
+##############
+# Assignment 4
+##############
+
+def displayMenu():
+    print("Please select a number from the following: \n",
+        "\n\t1. Singly Linked List\n",
+        "\t2. Check if Palindrome\n",
+        "\t3. Priority Queue\n",
+        "\t4. Evaluate an Infix Expression\n",
+        "\t5. Graph\n",
+        "\t6. Exit"
+        )
+
+=======
+#####
+# Assignment 4
+>>>>>>> c59196432879e1adbb05c0c7c17b3e4d36fdd726
