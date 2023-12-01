444810d52c663a1fe2a1f8d0459954d7e5675c94
GhiwaDaccache <ghiwadaccache4@gmail.com>
2023-12-01 23:41:07 +0200

    Binary Tree code started

--- Practice/Practice - Binary Trees.py
+++ Practice/Practice - Binary Trees.py
@@ -8,3 +8,39 @@ class Node:
         self.value = value
         self.left = None
         self.right = None 
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+        
+    def insert(self, info):
+        node = Node(info)
+        
+        if self.root == None:
+            self.root = node
+            print("Added", node.value, "as the root node.")
+        
+        else:
+            queue = [self.root]
+            while queue:
+                current = queue.pop(0)
+                
+                if current.left == None:
+                    current.left = node
+                    print("Added", node.value, "as the left child of", current.value)
+                    print()
+                    return
+                
+                elif current.right == None:
+                    current.right = node
+                    print("Added", node.value, "as the right child of", current.value)
+                    print()
+                    return
+                
+                else:
+                    queue.append(current.left)
+                    queue.append(current.right)
+                    print("Added", current.left.value, "and", current.right.value, "to the queue.")
+                    print()
+                    
+                
